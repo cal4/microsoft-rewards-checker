5e32f4a42f4e97f59bdfcb21a8f52cb7e2fe40f7
Cal Williams <9409256+cal4@users.noreply.github.com>
2024-04-29 15:50:13 -0400

    Change subject and add content

--- conftest.py
+++ conftest.py
@@ -20,8 +20,9 @@ def pytest_sessionfinish(session, exitstatus):
     smtp_server = "smtp.gmail.com"
     sender_email = os.getenv("FROM_EMAIL")
     email_message = EmailMessage()
-    email_message["Subject"] = "test_verifyallsearchesdone.py failed"
+    email_message["Subject"] = str(session)
     email_message["To"] = os.getenv("TO_EMAIL")
+    email_message.set_content(str(session.items))
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
